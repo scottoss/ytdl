b763468d200865c66418a47161eac527095ec9d5
derpyhooves <56843137+scottoss@users.noreply.github.com>
2022-12-28 21:19:38 +0100

    update

---
+++ Backup1/Services/SettingsService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using Tyrrrz.Settings;
+using YoutubeDownloader.Core.Downloading;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloader.Services;
+
+public partial class SettingsService : SettingsManager
+{
+    public bool IsAutoUpdateEnabled { get; set; } = true;
+
+    public bool IsDarkModeEnabled { get; set; } = IsDarkModeEnabledByDefault();
+
+    public bool ShouldInjectTags { get; set; } = true;
+
+    public bool ShouldSkipExistingFiles { get; set; }
+
+    public string FileNameTemplate { get; set; } = "$title";
+
+    public int ParallelLimit { get; set; } = 2;
+
+    public Container LastContainer { get; set; } = Container.Mp4;
+
+    public VideoQualityPreference LastVideoQualityPreference { get; set; } = VideoQualityPreference.Highest;
+
+    public SettingsService()
+    {
+        Configuration.StorageSpace = StorageSpace.Instance;
+        Configuration.SubDirectoryPath = "";
+        Configuration.FileName = "Settings.dat";
+    }
+}
+
+public partial class SettingsService
+{
+    private static bool IsDarkModeEnabledByDefault()
+    {
+        try
+        {
+            return Registry.CurrentUser.OpenSubKey(
+                "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+                false
+            )?.GetValue("AppsUseLightTheme") is 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
---
+++ Backup1/ViewModels/RootViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using MaterialDesignThemes.Wpf;
+using Stylet;
+using YoutubeDownloader.Services;
+using YoutubeDownloader.Utils;
+using YoutubeDownloader.ViewModels.Components;
+using YoutubeDownloader.ViewModels.Dialogs;
+using YoutubeDownloader.ViewModels.Framework;
+
+namespace YoutubeDownloader.ViewModels;
+
+public class RootViewModel : Screen
+{
+    private readonly IViewModelFactory _viewModelFactory;
+    private readonly DialogManager _dialogManager;
+    private readonly SettingsService _settingsService;
+    private readonly UpdateService _updateService;
+
+    public SnackbarMessageQueue Notifications { get; } = new(TimeSpan.FromSeconds(5));
+
+    public DashboardViewModel Dashboard { get; }
+
+    public RootViewModel(
+        IViewModelFactory viewModelFactory,
+        DialogManager dialogManager,
+        SettingsService settingsService,
+        UpdateService updateService)
+    {
+        _viewModelFactory = viewModelFactory;
+        _dialogManager = dialogManager;
+        _settingsService = settingsService;
+        _updateService = updateService;
+
+        Dashboard = _viewModelFactory.CreateDashboardViewModel();
+
+        DisplayName = $"{App.Name} v{App.VersionString}";
+    }
+
+    private async Task ShowWarInUkraineMessageAsync()
+    {
+        var dialog = _viewModelFactory.CreateMessageBoxViewModel(
+            "Ukraine is at war!", @"
+My country, Ukraine, has been invaded by Russian military forces in an act of aggression that can only be described as genocide.
+Be on the right side of history! Consider supporting Ukraine in its fight for freedom.
+
+Press LEARN MORE to find ways that you can help.".Trim(),
+            "LEARN MORE", "CLOSE"
+        );
+
+        if (await _dialogManager.ShowDialogAsync(dialog) == true)
+        {
+            ProcessEx.StartShellExecute("https://tyrrrz.me/ukraine?source=youtubedownloader");
+        }
+    }
+
+    private async Task CheckForUpdatesAsync()
+    {
+        try
+        {
+            var updateVersion = await _updateService.CheckForUpdatesAsync();
+            if (updateVersion is null)
+                return;
+
+            Notifications.Enqueue($"Downloading update to {App.Name} v{updateVersion}...");
+            await _updateService.PrepareUpdateAsync(updateVersion);
+
+            Notifications.Enqueue(
+                "Update has been downloaded and will be installed when you exit",
+                "INSTALL NOW", () =>
+                {
+                    _updateService.FinalizeUpdate(true);
+                    RequestClose();
+                }
+            );
+        }
+        catch
+        {
+            // Failure to update shouldn't crash the application
+            Notifications.Enqueue("Failed to perform application update");
+        }
+    }
+
+    public async void OnViewFullyLoaded()
+    {
+        await ShowWarInUkraineMessageAsync();
+        await CheckForUpdatesAsync();
+    }
+
+    protected override void OnViewLoaded()
+    {
+        base.OnViewLoaded();
+
+        _settingsService.Load();
+
+        if (_settingsService.IsDarkModeEnabled)
+        {
+            App.SetDarkTheme();
+        }
+        else
+        {
+            App.SetLightTheme();
+        }
+    }
+
+    protected override void OnClose()
+    {
+        base.OnClose();
+
+        Dashboard.CancelAllDownloads();
+
+        _settingsService.Save();
+        _updateService.FinalizeUpdate(false);
+    }
+}
